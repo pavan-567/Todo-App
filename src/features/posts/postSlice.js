c200a145cc2a3aed88f7ab8e63c84991c2198927
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2024-05-23 22:52:00 +0530

    Removed Default Styles + File Structure Modified For Redux Store

---
+++ src/features/posts/postSlice.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+
+const initialState = {
+  posts: [],
+};
+
+// Structure => {id: number, post: string, completed: boolean}
+
+export default function postReducer(state = initialState, action) {
+  switch (action.type) {
+    case "posts/createPost":
+      return { ...state, posts: [...state.posts, action.payload] };
+
+    case "posts/removePost":
+      const filteredPosts = state.posts.filter(
+        (post) => post.id !== action.payload
+      );
+      return { ...state, posts: filteredPosts };
+
+    case "posts/removeAllPosts":
+      return initialState;
+
+    default:
+      return state;
+  }
+}
+
+// Action Creators
+export function createPost(todo) {
+  return {
+    type: "posts/createPost",
+    payload: {
+      id: uuidv4(),
+      post: todo,
+      completed: false,
+    },
+  };
+}
+
+export function removePost(id) {
+  return { type: "posts/removePost", payload: id };
+}
+
+export function removeAllPosts() {
+  return { type: "posts/removeAllPosts" };
+}
