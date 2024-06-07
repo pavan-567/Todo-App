f56854582a310ba803f3716081a8e00107b8d836
Pavan Ram Gangadhar Kumar <gangathecoder425@gmail.com>
2024-06-07 10:56:39 +0530

    Added Title For The Website

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,9 +3,15 @@ import Div from "../features/todo/styles/Div";
 import Container from "../features/todo/styles/Container";
 import useFetchUser from "../hooks/useFetchUser";
 import Spinner from "../features/todo/styles/Spinner";
+import { useEffect } from "react";
 
 function AppLayout() {
   const loading = useFetchUser();
+
+  useEffect(() => {
+    document.title = "Todo | React";
+  }, []);
+
   return (
     <Div>
       <Container>
